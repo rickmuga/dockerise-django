1be6f2cceddadf584050caf2294dde189173293f
Patrick.Mugazambi <patrick.mugazambi@takealot.com>
2019-03-18 09:03:49 +0200

    Use py36 alpine image

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
-FROM python:3.7.2-alpine
+FROM python:3.6.7-alpine
 ENV PYTHONUNBUFFERED 1
+
+RUN apk update && \
+    apk add --virtual build-deps gcc python3-dev musl-dev && \
+    apk add mariadb-dev && \
+    apk add mariadb-client && \
+    apk add mysql && \
+    apk add mysql-client
+
 RUN mkdir /code
 WORKDIR /code
+
 ADD requirements.txt /code/
-RUN pip install -r requirements.txt
+RUN pip3 install --upgrade pip && pip3 install -r requirements.txt
 ADD ./ /code/
 # CMD ["python", "mysite/manage.py", "runserver", "0.0.0.0:8001"]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_d@=9n(ilo!%c-nzt01(w*p3&bfxw%1wy*txu+h^&on)!elr=u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
@@ -73,13 +73,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+MYSQL_ROOT_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',                      
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'mytestdb',
+        'PORT': '3306',
+        'OPTIONS': {
+          'autocommit': True,
+        }, 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
